d3eeb6c86214d1bcd3541b550967fb704e6c976e
Samuel Coleman <smcoleman19@gmail.com>
2019-10-23 19:50:54 -0600

    Add basic functionality for displaying the overview on card click

--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './MovieCard.css';
 
-const MovieCard = ({ title, poster_path }) => {
+const MovieCard = ({ title, poster_path, overview, id }) => {
 
     return (
-        <section className='movie_card'>
+        <section className='movie_card' onClick={() => document.getElementById(id).removeAttribute('hidden')}>
             <h1>{ title }</h1>
             <img src={poster_path} />
             <button type='button'>Favorite</button>
+            <p id={id} hidden>{ overview }</p>
         </section>
     )
 }
