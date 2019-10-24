8f12c7b38763c295e1a6feba197f8b15edac0901
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-10-23 19:32:10 -0600

    Add snapshot test to MovieCard component

--- src/Components/MovieCard/MovieCard.test.js
+++ src/Components/MovieCard/MovieCard.test.js
@@ -1,1 +1,25 @@
- 
+ import React from 'react';
+ import { shallow } from 'enzyme';
+ import MovieCard from './MovieCard';
+
+ describe('MovieCard', () => {
+
+    let wrapper;
+    let chosenMovie =  {
+        id: 456,
+        title: 'Harry Potter and the Sorceres Stone',
+        popularity: 462.91,
+        vote_count: 3775,
+        video: false,
+      }
+
+    beforeEach(() => {
+      wrapper = shallow(<MovieCard 
+         {...chosenMovie}
+      />)
+    })
+
+    it('should match snapshot with all data passing through correctly', () => {
+        expect(wrapper).toMatchSnapshot();
+    })
+ })
