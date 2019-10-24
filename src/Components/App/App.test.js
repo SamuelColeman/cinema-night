066993eda1e9d6a6bafd812cbd4c6e3889260810
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-10-23 20:07:43 -0600

    Start in App testing but need to go over redux testing to continue the test

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -10,7 +10,44 @@ describe('App', () => {
   let wrapper;
   let mockCurrentMovie;
 
-  
+  beforeEach(() => {
+    wrapper = shallow(<App />)
+
+    mockCurrentMovie = {
+      popularity: 462.91,
+      vote_count: 3775,
+      video: false,
+      poster_path: "/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg",
+      id: 475557,
+      adult: false,
+      backdrop_path: "/n6bUvigpRFqSwmPp1m2YADdbRBc.jpg",
+      original_language: "en",
+      original_title: "Joker",
+      genre_ids: [ 80, 18, 53 ],
+      title: "Joker",
+      vote_average: 8.6,
+      overview: "During the 1980s, a failed stand-up comedian is driven insane and turns to a life of crime and chaos in Gotham City while becoming an infamous psychopathic crime figure.",
+      release_date: "2019-10-04"
+      }
+
+      currentMovies.mockImplementation(() => {
+        return Promise.resolve(mockCurrentMovie);
+      }); 
+  })
+
+  it('should match snapshot with correct data passing throught', () => {
+    expect(wrapper).toMatchSnapshot();
+  })
+
+  it('should call getMovies when componentDidMount is called', () => {
+    wrapper.instance().componentDidMount();
+
+    expect(currentMovies).toHaveBeenCalled();
+  })
+
+  // test isLoading to have been called
+  // test hasError to have been called
+  // test change of state, will need to go over redux testing class
 })
 
  
