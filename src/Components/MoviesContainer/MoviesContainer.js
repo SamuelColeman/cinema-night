a6fb57187063a314c124b436beb34738e6a98e8a
Zoe <zoekharvey@gmail.com>
2019-10-26 00:29:00 -0600

    Add additional styling to cards

--- src/Components/MoviesContainer/MoviesContainer.js
+++ src/Components/MoviesContainer/MoviesContainer.js
@@ -3,6 +3,7 @@ import MovieCard from '../MovieCard/MovieCard';
 import './MoviesContainer.css';
 import { NavLink, Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+
 // import login from '../../images/login.png';
 // import logo from '../../images/title.png'
 
@@ -32,20 +33,18 @@ const MoviesContainer = ({ currentUser, movies, signOutUser, removeFavourite }) 
     return (
         <section className='movies-containers'>
           <header>
-          <div className="div__movie--header">
-          {/* <img className="img--logo" src={logo} alt="Cinema Night logo"/> */}
           <h1 className="h1-cinema-night">CINEMA NIGHT</h1>
           <div className="div__movie--title">
-        <h6 className="h6--playing title">Now Playing</h6>
+          <h6 className="h6--playing title">Now Playing</h6>
+          <h4 className="h4--view-favorites title-link">View Favorites</h4>
+        
         {button}
-        {/* <h4 className="h4--login title-button">{logInButton}</h4> */}
-        <h4 className="h4--view-favorites title-link">View Favorites</h4>
+
         </div>
-      
-          {/* <img className="img--login" src={login} alt="log in"/> */}
-          </div>
           </header>
+          <div className="movies">
           {loopMovies}
+          </div>
         </section>
     ) 
 } 
